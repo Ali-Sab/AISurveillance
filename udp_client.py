810d7448532cc2c60614565c3cfddebbe04ced5b
Alison Sabuwala <alison.sabuwala1024@gmail.com>
2023-11-08 02:05:09 -0500

    added timeout handler, error handler, 2 second loop. timeout is set to 2 seconds. works and survives timeouts successfully

--- udp_client.py
+++ udp_client.py
@@ -8,13 +8,15 @@ bytes_to_send = str.encode(passcode)
 server_address_port = ("10.0.0.44", 5030)
 buffer_size = 1436
 fd = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+fd.settimeout(2)
 
 def get_pic():
   fd.sendto(bytes_to_send, server_address_port)
 
   msg = fd.recvfrom(buffer_size)
-  if msg[0] == "ERROR":
-     return
+  if len(msg[0]) == 5 and msg[0].decode('ASCII') == "ERROR":
+    print("ERROR at {}".format(datetime.now().strftime('%Y.%m.%d_%H.%M.%S')))
+    return
   
   msg_length = len(msg[0])
   image = msg[0]
@@ -26,15 +28,20 @@ def get_pic():
     image += msg[0]
     print("Received {} bytes".format(msg_length))
 
-  img_data = image
   filename = 'recv_img_{}.jpg'.format(datetime.now().strftime('%Y.%m.%d_%H.%M.%S'))
 
   with open(filename, 'wb') as f:
-      f.write(img_data)
+    f.write(image)
+  
+  print("Wrote image of size {} B".format(len(image)))
+  if len(image) < 100:
+    print(image)
 
-get_pic()
 
 while True:
-  get_pic()
+  try:
+    get_pic()
+  except socket.timeout:
+    print("TIME OUT at {}".format(datetime.now().strftime('%Y.%m.%d_%H.%M.%S')))
   time.sleep(2)
    
