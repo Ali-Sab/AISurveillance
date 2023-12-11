9a23cb6060aa5c69aa7aaeee2b51cf4ad9f8e573
Alison Sabuwala <alison.sabuwala1024@gmail.com>
2023-12-11 02:37:15 -0500

    added slider with ability to check which value is set

--- frontend/src/components/Live/index.jsx
+++ frontend/src/components/Live/index.jsx
@@ -1,6 +1,54 @@
+import Slider from '@mui/material/Slider';
+import Image from 'react-bootstrap/Image';
+import { useState } from 'react';
+
+const marks = [
+    {
+        value: 0,
+        label: '0°C',
+    },
+    {
+        value: 20,
+        label: '20°C',
+    },
+    {
+        value: 37,
+        label: '37°C',
+    },
+    {
+        value: 100,
+        label: '100°C',
+    },
+];
+
+function valuetext(value) {
+    return `${value}°C`;
+}
+
+function valueLabelFormat(value) {
+    return marks.findIndex((mark) => mark.value === value) + 1;
+}
+
 function Live(props) {
+    const [imagePath, setImagePath] = useState([]);
+
+    function updateImage(value) {
+        setImagePath(value.target.value);
+    }
+
     return <div>
-        Live
+        {/* <Image src={imagePath} /> */}
+        <div>Something: {imagePath}</div>
+        <Slider
+            aria-label="Restricted values"
+            defaultValue={20}
+            valueLabelFormat={valueLabelFormat}
+            getAriaValueText={valuetext}
+            step={null}
+            valueLabelDisplay="auto"
+            marks={marks}
+            onChange={updateImage}
+        />
     </div>
 }
 
