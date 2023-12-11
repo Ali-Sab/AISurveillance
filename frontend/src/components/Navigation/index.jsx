0acd9f80d58e612dcaae56d1ace6da59dcfc13e6
Alison Sabuwala <alison.sabuwala1024@gmail.com>
2023-12-11 02:16:22 -0500

    added basic navbar

--- frontend/src/components/Navigation/index.jsx
+++ frontend/src/components/Navigation/index.jsx
@@ -1,6 +1,27 @@
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import NavbarBrand from 'react-bootstrap/esm/NavbarBrand';
+import LinkContainer from 'react-router-bootstrap/LinkContainer';
+import Nav from 'react-bootstrap/Nav';
+import { NavLink } from 'react-router-dom';
+
 function Navigation(props) {
     return <div>
-        Navigation
+        <Navbar>
+            <Container fluid>
+                <LinkContainer to="/">
+                    <NavbarBrand>AISurveillance</NavbarBrand>
+                </LinkContainer>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse>
+                    <Nav className="me-auto">
+                        <Nav.Item><NavLink className="nav-link" to={"/live"}>Live</NavLink></Nav.Item>
+                        <Nav.Item><NavLink className="nav-link" to={"/recordings"}>Recordings</NavLink></Nav.Item>
+                        <Nav.Item><NavLink className="nav-link" to={"/archive"}>Archive</NavLink></Nav.Item>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     </div>
 }
 
