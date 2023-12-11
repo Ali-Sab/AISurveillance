824e897bef2d2827b3c9fed3b3a83f3bcd1ac1ec
Alison Sabuwala <alison.sabuwala1024@gmail.com>
2023-12-11 16:39:36 -0500

    added separate file to batch process yolov7, set it to every 20 images, so every minute Untested!

--- backend/http_client.py
+++ backend/http_client.py
@@ -2,85 +2,41 @@ import requests
 from datetime import datetime
 import time
 import subprocess
-import os
-import sqlite3
-from PIL import Image
-
-RESULT_PATH = "runs/detect/"
-OUTPUT_IMAGE_PATH = "res/"
+import logging 
 
 server_address = 'http://10.0.0.44/cam-hi.jpg'
 
-con = sqlite3.connect('image.db')
-cur = con.cursor()
+OUTPUT_IMAGE_PATH = "images/"
 
+logging.basicConfig(filename="logs/http_client.log", filemode='a', encoding='utf-8', level=logging.DEBUG)
 
+count = 0
 def get_pic():
   try:
     response = requests.get(server_address, timeout=3)
   except (requests.exceptions.Timeout, requests.exceptions.ReadTimeout, requests.exceptions.ConnectionError) as e:
-    print("TIMEOUT at {}".format(datetime.now().strftime('%Y.%m.%d_%H.%M.%S')))
+    logging.error("TIMEOUT at {}".format(datetime.now().strftime('%Y.%m.%d_%H.%M.%S')))
+    logging.error(e)
     return
   
   if response.status_code != 200:
-    print("ERROR at {}".format(datetime.now().strftime('%Y.%m.%d_%H.%M.%S')))
-    print("ERROR CODE: ", response.status_code)
-    print(response.content())
+    logging.error("ERROR at {}".format(datetime.now().strftime('%Y.%m.%d_%H.%M.%S')))
+    logging.error("ERROR CODE: ", response.status_code)
+    logging.error(response.content())
     return
     
   image = response.content
-  print("Received {} bytes".format(len(image)))
+  logging.debug("Received {} bytes".format(len(image)))
 
   filename = 'recv_img_{}.jpg'.format(datetime.now().strftime('%Y.%m.%d_%H.%M.%S'))
-  filename_raw = filename[:-4]
 
-  with open('images/' + filename, 'wb') as f:
+  with open(OUTPUT_IMAGE_PATH + filename, 'wb') as f:
     f.write(image)
   
-  print("Wrote image of size {} B".format(len(image)))
-
-  # Run YoloV7 on the image here
-  subprocess.run(["python3", "yolov7/detect.py", "--weights", "yolov7/yolov7-tiny.pt", "--conf", "0.25", "--source", 'images/' + filename, "--save-txt", "--name", filename_raw, "--no-trace"]) 
-  subprocess.run(['rm', 'images/' + filename])
-
-  # text file path
-  # print(RESULT_PATH + filename_raw + '/labels/' + filename_raw + '.txt')
-  # image path (after yolov7)
-  # print(RESULT_PATH + filename)
-
-  # Check result
-  if os.path.exists(RESULT_PATH + filename_raw + '/labels/' + filename_raw + '.txt'):
-    print("Opening txt at path={}".format(RESULT_PATH + filename_raw + '/labels/' + filename_raw + '.txt'))
-    with open(RESULT_PATH + filename_raw + '/labels/' + filename_raw + '.txt', 'r') as file:
-      personFound = False
-      while True:
-        line = file.readline()
-        if not line:
-          break
-        values = line.split(' ')
-        if values[0] == '0':
-          personFound = True
-          break
-          
-      if personFound:
-        filename_new = filename[:-4] + '_person.jpg'
-        print("Moving image with person to {}".format(OUTPUT_IMAGE_PATH + 'flagged/' + filename_new))
-        subprocess.run(['mv', RESULT_PATH + filename_raw + '/' + filename, OUTPUT_IMAGE_PATH + 'flagged/' + filename_new])
-        cur.execute("INSERT INTO image VALUES(?, ?, ?)", (OUTPUT_IMAGE_PATH + 'flagged/' + filename_new, datetime.now(), True))
-      else:
-        print("Moving image to {}".format(OUTPUT_IMAGE_PATH + 'unflagged/' + filename))
-        subprocess.run(['mv', RESULT_PATH + filename_raw + '/' + filename, OUTPUT_IMAGE_PATH + 'unflagged/' + filename])
-        cur.execute("INSERT INTO image VALUES(?, ?, ?)", (OUTPUT_IMAGE_PATH + 'unflagged/' + filename, datetime.now(), False))
-        print("Compressing image")
-        subprocess.run(['jpegoptim', '--size=6k',  OUTPUT_IMAGE_PATH + 'unflagged/' + filename])
-  else:
-    # No txt file found -> nothing was found in the picture
-    print("Moving image to {}".format(OUTPUT_IMAGE_PATH + 'unflagged/' + filename))
-    subprocess.run(['mv', RESULT_PATH + filename_raw + '/' + filename, OUTPUT_IMAGE_PATH + 'unflagged/' + filename])
-    cur.execute("INSERT INTO image VALUES(?, ?, ?)", (OUTPUT_IMAGE_PATH + 'unflagged/' + filename, datetime.now(), False))
-    print("Compressing image")
-    subprocess.run(['jpegoptim', '--size=6k',  OUTPUT_IMAGE_PATH + 'unflagged/' + filename])
-  con.commit()
+  logging.debug("Wrote image of size {} B".format(len(image)))
+  
+  global count
+  count += 1
 
   # How do I delete it automatically in 30 days?
   #  Add a delete command in the main loop that does datetime.now() - 30 days, and checks filename at the current second and second-1
@@ -91,4 +47,7 @@ def get_pic():
 
 while True:  
   get_pic()
-  time.sleep(1)
+  time.sleep(3)
+  if count >= 20:
+    subprocess.Popen(['python3', 'run_yolov7.py'])
+    count = 0
---
+++ backend/run_yolov7.py
@@ -0,0 +1,72 @@
+import subprocess
+from datetime import datetime
+import os
+from os import listdir
+from os.path import isfile, join
+import sqlite3
+import logging
+
+con = sqlite3.connect('image.db')
+cur = con.cursor()
+
+RESULT_PATH = "runs/detect/"
+OUTPUT_IMAGE_PATH = "res/"
+INPUT_IMAGE_PATH = "images/"
+formattedTime = datetime.now().strftime('%Y.%m.%d')
+
+logging.basicConfig(filename="logs/run_yolov7_" + formattedTime + ".log", filemode='a', encoding='utf-8', level=logging.DEBUG)
+
+formattedTime = datetime.now().strftime('%Y.%m.%d_%H.%M.%S')
+
+onlyfiles = [f for f in listdir(INPUT_IMAGE_PATH) if isfile(join(INPUT_IMAGE_PATH, f))]
+logging.debug("Found the following images: {}".format(onlyfiles))
+
+imgPath = 'image_set_' + formattedTime
+subprocess.run(['mkdir', imgPath])
+for filename in onlyfiles:
+	subprocess.run(['mv', 'images/' + filename, imgPath])
+
+expName = "exp_" + formattedTime
+expPath = RESULT_PATH + expName
+
+
+subprocess.run(["python3", "yolov7/detect.py", "--weights", "yolov7/yolov7-tiny.pt", "--conf", "0.25", "--source", imgPath, "--save-txt", "--name", expName, "--no-trace"])
+subprocess.run(['rm', '-rf', imgPath])
+
+onlyfiles = [f for f in listdir(expPath) if isfile(join(expPath, f))]
+logging.debug("Processed these images: {}".format(onlyfiles))
+
+for filename in onlyfiles:
+	logging.debug("Checking file: {}".format(filename))
+	dt = datetime.strptime(filename, 'recv_img_%Y.%m.%d_%H.%M.%S.jpg')
+	filename_raw = filename[:-4]
+
+	# Check result
+	personFound = False
+	if os.path.exists(expPath + '/labels/' + filename_raw + '.txt'):
+		logging.debug("Opening txt at path: {}".format(expPath + '/labels/' + filename_raw + '.txt'))
+		with open(expPath + '/labels/' + filename_raw + '.txt', 'r') as file:
+			personFound = False
+			while True:
+				line = file.readline()
+				if not line:
+					break
+				values = line.split(' ')
+				if values[0] == '0':
+					personFound = True
+					break
+            
+	# If no txt file found, then no person in image
+	if personFound:
+			filename_new = filename_raw + '_person.jpg'
+			logging.debug("Moving image with person to {}".format(OUTPUT_IMAGE_PATH + 'flagged/' + filename_new))
+			subprocess.run(['mv', expPath + "/" + filename, OUTPUT_IMAGE_PATH + 'flagged/' + filename_new])
+			cur.execute("INSERT INTO image VALUES(?, ?, ?)", (OUTPUT_IMAGE_PATH + 'flagged/' + filename_new, datetime.now(), True))
+	else:
+			logging.debug("Moving image to {}".format(OUTPUT_IMAGE_PATH + 'unflagged/' + filename))
+			subprocess.run(['mv', expPath + "/" + filename, OUTPUT_IMAGE_PATH + 'unflagged/' + filename])
+			cur.execute("INSERT INTO image VALUES(?, ?, ?)", (OUTPUT_IMAGE_PATH + 'unflagged/' + filename, datetime.now(), False))
+			logging.debug("Compressing image")
+			subprocess.run(['jpegoptim', '--size=6k',  OUTPUT_IMAGE_PATH + 'unflagged/' + filename])
+
+	con.commit()
